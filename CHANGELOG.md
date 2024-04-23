ed91095ec6fe0e12eb7615deb537ab6ad5e6a93e
Uriberto Lopez <urilopez891@gmail.com>
2024-04-22 20:37:01 -0700

    Submitting assignment.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# CHANGELOG
+
+## 4/22/2024
+### Contributors
+Uriberto Lopez
+
+### Added
+04/22/24: Added the images finished case and video of working VU meter. 
+
+### Changed
+04/22/24: Addded CHANGELOG file.
+
+### Removed
+None.
--- main.py
+++ main.py
@@ -10,10 +10,17 @@ status = DigitalInOut(board.IO17)
 status.direction = Direction.OUTPUT
 
 led_pins = [
-    board.IO21,
-    board.IO26, # type: ignore
-    board.IO47,
-    # do the rest...
+    board.IO21,  # type: ignore
+    board.IO26,  # type: ignore
+    board.IO47,  # type: ignore
+    board.IO33,  # type: ignore
+    board.IO34,  # type: ignore
+    board.IO48,  # type: ignore
+    board.IO35,  # type: ignore
+    board.IO36,  # type: ignore
+    board.IO37,  # type: ignore
+    board.IO38,  # type: ignore
+    board.IO39   # type: ignore
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -26,12 +33,25 @@ while True:
     volume = microphone.value
 
     print(volume)
+    if (volume > 70000):
+        for i in range(11):
+            leds[i].value = 1
+    elif (volume > 60000):
+        for i in range(9):
+            leds[i].value = 1
+    elif (volume > 50000):
+        for i in range(7):
+            leds[i].value = 1
+    elif (volume > 40000):
+        for i in range(5):
+            leds[i].value = 1
+    elif (volume > 30000):
+        for i in range(3):
+            leds[i].value = 1
+    elif (volume > 20000):
+        for i in range(1):
+            leds[i].value = 1
+    else:
+        for i in range(11):
+            leds[i].value = 0
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
-
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
